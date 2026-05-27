1b1bf95f89c5fc8cca3ee28eabe5add5c8226065
24f1002114 <24f1002114@ds.study.iitm.ac.in>
2026-05-27 03:52:17 +0000

    fix: remove caching, fix patient role check, fix CSV cleanup

--- application/resources.py
+++ application/resources.py
@@ -1181,7 +1181,7 @@ api.add_resource(BlacklistResource, '/api/user/<int:id>/blacklist')
 api.add_resource(DepartmentResource, "/api/departments", "/api/department/<int:id>")
 api.add_resource(AppointmentResource, "/api/appointments", "/api/appointment/<int:appointment_id>")
 api.add_resource(TreatmentResource, "/api/treatments", "/api/treatment/<int:treatment_id>")
-api.add_resource(AvailabilityResource, "/api/availabilities", "/api/availabilities/<int:doctor_id>", "/api/doctor/availability/<int:availability_id>")
+api.add_resource(AvailabilityResource, "/api/availabilities", "/api/availability/<int:doctor_id>", "/api/doctor/availability/<int:availability_id>")
 api.add_resource(DoctorSearchResource, "/api/search/doctors")
 api.add_resource(PatientSearchResource, "/api/search/patients")
 
